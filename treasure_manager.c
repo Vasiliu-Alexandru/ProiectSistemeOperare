8b2b8448ddbccd76097d04fd15cc51edec9c42c9
Vasiliu-Alexandru <vasiliu_alexandru@hotmail.com>
2025-04-11 20:21:34 +0300

    commit

--- treasure_manager.c
+++ treasure_manager.c
@@ -4,6 +4,7 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #define MAX_USERNAME 64 // lungime maxima username (63 caractere + terminator null)
 #define MAX_CLUE 256    // lungime maxima username (255 caractere + terminator null)
@@ -17,49 +18,73 @@ typedef struct {
     int   value;
 }Treasure;
 
-
-
-int main(int argc, char **argv)
+void argument_error(void)
 {
-  char *buffer=malloc(4096*sizeof(char *));
-  char *mesaj="Please include file name as an argument\n";
-  if(argc!=2)
-    {
-      write(1,mesaj,strlen(mesaj));
-      free(buffer);
-      return -1;
-    }
+   const char mesajEroare[] ="Incorrect usage of program. To use treasure manager please input the desired operation which include the following:\n   add <hunt_id>\n   list <hunt_id>\n   view <hunt_id> <id>\n   remove_treasure <hunt_id> <id>\n   remove_hunt <hunt_id>\nFor a more detailed help guide include --help\n";
 
+   write(STDOUT_FILENO, mesajEroare,sizeof(mesajEroare));
+
+  return;
+}
+
+void help(void)
+{
+  const char mesaj[] ="To use treasure manager please input the desired operation which include the following:\n   add <hunt_id>                   -Adds a new treasure to the  hunt/game session specified by \"hunt_id\"\n   list <hunt_id>                  -Lists all treasures in the specified hunt\n   view <hunt_id> <id>             -View details of a specific treasure \n   remove_treasure <hunt_id> <id>  -Removes specified treasure from hunt\n   remove_hunt <hunt_id>           -Removes entire hunt, use with caution\n";
+  write(STDOUT_FILENO, mesaj,sizeof(mesaj));
+  return;
+
+}
+
+int este_director(const char *cale)
+{
   struct stat st;
-  if(stat(argv[1],&st)!=0)
+  if(stat(cale,&st)==-1)
+  {
+    return 0; //calea nu exista
+  }
+  return S_ISDIR(st.st_mode); // 1 daca este director 0 daca nu este
+}
+
+void add(char *hunt_id)
+{
+  if(este_director(hunt_id))
+    printf("Exista directorul\n");
+    else
+      printf("Nu exista directorul\n");
+
+}
+
+int main(int argc, char**argv)
+{
+  if(argc<2)
+  {
+    argument_error();
+    exit(EXIT_FAILURE);
+  }
+
+  for(int i=1;i<argc;i++)
+  {
+    if(strcmp(argv[i],"--help")==0)
     {
-      char *mesaj_eroare="Fisierul nu exista, dar s-a creat\n";
-      write(1,mesaj_eroare,strlen(mesaj_eroare));
-      int f;
-      f=open(argv[1], O_CREAT| O_RDWR| O_APPEND, S_IRUSR| S_IWUSR);
-      int bytes;
-       while((bytes=read(0,buffer, sizeof(buffer)))>0)
-	  {
-	      write(f,buffer,bytes);
-	  }
-
-       close(f);
-      
+      help();
+      exit(EXIT_SUCCESS);
     }
-  else
+  }
+
+  if(strcmp(argv[1],"--add")==0)
+  {
+    if(argc!=3)
     {
-        int fd;
-	fd=open(argv[1], O_RDONLY, 0777);
-
-	int bytesread;
-	while((bytesread=read(fd,buffer, sizeof(buffer)))>0)
-	    {
-	      write(1,buffer,bytesread);
-	    }
-
-	 close(fd);
+      argument_error();
+      exit(EXIT_FAILURE);
     }
-  free(buffer);
+    add(argv[2]);
+    exit(EXIT_SUCCESS);
+  }
+
+
+  argument_error();
+
   return 0;
 }
 
